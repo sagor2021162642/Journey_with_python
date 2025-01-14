201bdf7f71b3bb62649aa60e961792d138156e10
Sagor Mahmud Opu <70199417+sagor2021162642@users.noreply.github.com>
2025-01-14 14:19:00 +0600

    Update Pass_generator.py

--- Pass_generator.py
+++ Pass_generator.py
@@ -22,3 +22,23 @@ for iterator in range(0,nr_numbers):
     print(numbers[random.randint(0, (len(numbers)-1))])
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
